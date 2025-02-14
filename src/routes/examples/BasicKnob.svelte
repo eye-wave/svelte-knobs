7f8abf067ff189ac7856ec7e4c41ad90ecffa614
_eyewave <89079979+eye-wave@users.noreply.github.com>
2025-02-14 01:18:05 +0100

    update examples

---
+++ src/routes/examples/BasicKnob.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { SvgKnob } from '$lib';
+
+	let value = $state(0.0);
+</script>
+
+<div>
+	<SvgKnob bind:value />
+	<span>{value.toFixed(2)}</span>
+
+	<SvgKnob bind:value disabled />
+</div>
+
+<style>
+	div {
+		color: #22bfee;
+	}
+</style>
---
+++ src/routes/examples/EnumParam.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { BoolParam, EnumParam } from '$lib/params';
+	import SvgKnob from '$lib/SvgKnob.svelte';
+
+	let value = $state(0);
+
+	const fruitParam = new EnumParam(['🍍', '🍉', '🍌', '🍋', '🍇'] as const);
+	const filterTypeParam = new EnumParam([
+		'Low pass',
+		'High pass',
+		'Low shelf',
+		'High shelf',
+		'Bell',
+		'Notch',
+		'Allpass'
+	] as const);
+
+	const booleanParam = new BoolParam();
+</script>
+
+<SvgKnob
+	bind:value
+	snapPoints={fruitParam.snapPoints}
+	snapThreshold={fruitParam.snapThreshold}
+	step={fruitParam.step}
+/>
+<p>{fruitParam.denormalize(value)}</p>
+
+<SvgKnob bind:value {...filterTypeParam.knobProps} />
+<p>{filterTypeParam.denormalize(value)}</p>
+
+<SvgKnob bind:value {...booleanParam.knobProps} />
+<p>{booleanParam.denormalize(value)}</p>
